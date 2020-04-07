2b5006694a7b70c1894e018b984be3f407d8749a
claash <makedonskiy.o.e@gmail.com>
2020-04-07 21:12:02 +0300

    is inline svg a file

--- app.js
+++ app.js
@@ -1,28 +1,28 @@
 document.addEventListener('DOMContentLoaded', () => {
-	const intMap = new IntMap({
-		color: '#003B61',
-		fontFamily: 'Arial',
-		multiple: 'horizontal',
-		marker: {
-			url: 'http://127.0.0.1:8080/marker.svg',
-			height: '16px',
-			width: '16px'
-		}
-	});
+    const intMap = new IntMap({
+        color: '#003B61',
+        fontFamily: 'Arial',
+        multiple: 'horizontal',
+        marker: {
+            url: 'http://127.0.0.1:8080/marker.svg',
+            height: '16px',
+            width: '16px'
+        }
+    });
 
-	intMap.inlineSvg('.ukrmap').then((values) => {
-		intMap.setText('#Kyiv', 'Kyiv');
+    intMap.inlineSvg('.ukrmap').then((values) => {
+        intMap.setText('#Kyiv', 'Kyiv');
 
-		intMap.setMarker('#Kyiv');
-		intMap.setMarker('#Kyiv');
+        intMap.setMarker('#Kyiv');
+        intMap.setMarker('#Kyiv');
 
-		intMap.setText('#Lutsk', 'Lutsk');
+        intMap.setText('#Lutsk', 'Lutsk');
 
-		intMap.setMarker('#Lutsk');
-		intMap.setMarker('#Lutsk');
-		intMap.setMarker('#Lutsk');
-		intMap.setMarker('#Lutsk');
+        intMap.setMarker('#Lutsk');
+        intMap.setMarker('#Lutsk');
+        intMap.setMarker('#Lutsk');
+        intMap.setMarker('#Lutsk');
 
-		intMap.setText('#Lutsk', 'Lviv');
-	});
+        intMap.setText('#Lutsk', 'Lviv');
+    });
 });
--- intMap.js
+++ intMap.js
@@ -1,230 +1,232 @@
-const IntMap = function(params) {
-	const self = this;
-	this.color = params.color;
-	this.fontSize = params.fontSize;
-	this.fontWeight = params.fontWeight;
-	this.fontFamily = params.fontFamily;
-	this.multiple = typeof params.multiple !== 'undefined' ? params.multiple : false;
-	this.marker = params.marker;
+const IntMap = function (params) {
+    const self = this;
+    this.color = params.color;
+    this.fontSize = params.fontSize;
+    this.fontWeight = params.fontWeight;
+    this.fontFamily = params.fontFamily;
+    this.multiple = typeof params.multiple !== 'undefined' ? params.multiple : false;
+    this.marker = params.marker;
 
-	this.inlineSvg = (elemName) => {
-		this.elemName = elemName;
-		this.promises = [];
+    this.inlineSvg = (elemName) => {
+        this.elemName = elemName;
+        this.promises = [];
 
-		document.querySelectorAll(this.elemName).forEach((el) => {
-			this.promises.push(
-				new Promise((resolve) => {
-					const imgID = el.getAttribute('id');
-					const imgClass = el.getAttribute('class');
-					const imgURL = el.getAttribute('src');
+        document.querySelectorAll(this.elemName).forEach((el) => {
+            this.promises.push(
+                new Promise((resolve) => {
+                    const imgID = el.getAttribute('id');
+                    const imgClass = el.getAttribute('class');
+                    const imgURL = el.getAttribute('src');
 
-					fetch(imgURL).then((r) => r.text()).then((data) => {
-						const parser = new DOMParser();
-						const xmlDoc = parser.parseFromString(data, 'text/html');
-						const svg = xmlDoc.querySelector('svg');
+                    if (imgURL.indexOf('.') == -1) return;
 
-						if (typeof imgID !== 'undefined' && imgID !== null) {
-							svg.setAttribute('id', imgID);
-						}
+                    fetch(imgURL).then((r) => r.text()).then((data) => {
+                        const parser = new DOMParser();
+                        const xmlDoc = parser.parseFromString(data, 'text/html');
+                        const svg = xmlDoc.querySelector('svg');
 
-						if (typeof imgClass !== 'undefined' && imgClass !== null) {
-							svg.setAttribute('class', imgClass + ' replaced-svg');
-						}
+                        if (typeof imgID !== 'undefined' && imgID !== null) {
+                            svg.setAttribute('id', imgID);
+                        }
 
-						svg.removeAttribute('xmlns:a');
+                        if (typeof imgClass !== 'undefined' && imgClass !== null) {
+                            svg.setAttribute('class', imgClass + ' replaced-svg');
+                        }
 
-						el.parentNode.replaceChild(svg, el);
+                        svg.removeAttribute('xmlns:a');
 
-						resolve(svg);
-					});
-				})
-			);
-		});
+                        el.parentNode.replaceChild(svg, el);
 
-		return Promise.all(this.promises);
-	};
+                        resolve(svg);
+                    });
+                })
+            );
+        });
 
-	this.getPosition = (elem) => {
-		return document.querySelector(elem).getBBox();
-	};
+        return Promise.all(this.promises);
+    };
 
-	this.setText = (id, text, position, marker) => {
-		const element = document.createElementNS('http://www.w3.org/2000/svg', 'text'),
-			path = document.querySelector(id),
-			pos = self.getPosition(id);
+    this.getPosition = (elem) => {
+        return document.querySelector(elem).getBBox();
+    };
 
-		var prevElement = path.querySelector('text');
+    this.setText = (id, text, position, marker) => {
+        const element = document.createElementNS('http://www.w3.org/2000/svg', 'text'),
+            path = document.querySelector(id),
+            pos = self.getPosition(id);
 
-		if (document.body.contains(prevElement)) {
-			prevElement.textContent = text;
-			return;
-		}
+        var prevElement = path.querySelector('text');
 
-		let textPos = {};
+        if (document.body.contains(prevElement)) {
+            prevElement.textContent = text;
+            return;
+        }
 
-		const txt = document.createTextNode(text);
+        let textPos = {};
 
-		element.appendChild(txt);
-		path.appendChild(element);
+        const txt = document.createTextNode(text);
 
-		const elementSize = element.getBBox();
+        element.appendChild(txt);
+        path.appendChild(element);
 
-		switch (position) {
-			case 'bottom':
-				const patentBt = {
-					x: pos.x + pos.width / 2,
-					y: pos.y + pos.height
-				};
+        const elementSize = element.getBBox();
 
-				textPos = {
-					x: patentBt.x - elementSize.width / 2,
-					y: patentBt.y - elementSize.height / 2
-				};
-				break;
-			case 'top':
-				const patentTop = {
-					x: pos.x + pos.width / 2,
-					y: pos.y + elementSize.height
-				};
+        switch (position) {
+            case 'bottom':
+                const patentBt = {
+                    x: pos.x + pos.width / 2,
+                    y: pos.y + pos.height
+                };
 
-				textPos = {
-					x: patentTop.x - elementSize.width / 2,
-					y: patentTop.y - elementSize.height / 2
-				};
-				break;
-			default:
-				const parentMiddle = {
-					x: pos.x + pos.width / 2,
-					y: pos.y + pos.height / 2
-				};
+                textPos = {
+                    x: patentBt.x - elementSize.width / 2,
+                    y: patentBt.y - elementSize.height / 2
+                };
+                break;
+            case 'top':
+                const patentTop = {
+                    x: pos.x + pos.width / 2,
+                    y: pos.y + elementSize.height
+                };
 
-				textPos = {
-					x: parentMiddle.x - elementSize.width / 2,
-					y: parentMiddle.y - elementSize.height / 2
-				};
-				break;
-		}
+                textPos = {
+                    x: patentTop.x - elementSize.width / 2,
+                    y: patentTop.y - elementSize.height / 2
+                };
+                break;
+            default:
+                const parentMiddle = {
+                    x: pos.x + pos.width / 2,
+                    y: pos.y + pos.height / 2
+                };
 
-		element.style.fill = self.color;
-		element.style.fontSize = self.fontSize;
-		element.style.fontFamily = self.fontFamily;
-		element.style.fontWeight = self.fontWeight;
-		element.setAttributeNS(null, 'x', textPos.x);
-		element.setAttributeNS(null, 'y', textPos.y);
+                textPos = {
+                    x: parentMiddle.x - elementSize.width / 2,
+                    y: parentMiddle.y - elementSize.height / 2
+                };
+                break;
+        }
 
-		if (marker) {
-			self.textMarker(element);
-		}
-	};
+        element.style.fill = self.color;
+        element.style.fontSize = self.fontSize;
+        element.style.fontFamily = self.fontFamily;
+        element.style.fontWeight = self.fontWeight;
+        element.setAttributeNS(null, 'x', textPos.x);
+        element.setAttributeNS(null, 'y', textPos.y);
 
-	this.textMarker = (element) => {
-		const textPos = element.getBBox(),
-			parentId = element.parentNode.id;
+        if (marker) {
+            self.textMarker(element);
+        }
+    };
 
-		let markerPos = {};
+    this.textMarker = (element) => {
+        const textPos = element.getBBox(),
+            parentId = element.parentNode.id;
 
-		markerPos.x = textPos.x + textPos.width / 2 - parseInt(self.marker.width) / 2;
-		markerPos.y = textPos.y + textPos.height + parseInt(self.marker.height) / 2;
+        let markerPos = {};
 
-		self.setMarker(
-			'#' + parentId,
-			self.marker.url,
-			self.marker.height,
-			self.marker.width,
-			markerPos.x,
-			markerPos.y
-		);
-	};
+        markerPos.x = textPos.x + textPos.width / 2 - parseInt(self.marker.width) / 2;
+        markerPos.y = textPos.y + textPos.height + parseInt(self.marker.height) / 2;
 
-	this.setMarker = (id, url, height, width, x, y) => {
-		const element = document.createElementNS('http://www.w3.org/2000/svg', 'image'),
-			path = document.querySelector(id),
-			pos = self.getPosition(id);
+        self.setMarker(
+            '#' + parentId,
+            self.marker.url,
+            self.marker.height,
+            self.marker.width,
+            markerPos.x,
+            markerPos.y
+        );
+    };
 
-		if (!this.multiple) {
-			var prevElement = path.querySelector('image');
+    this.setMarker = (id, url, height, width, x, y) => {
+        const element = document.createElementNS('http://www.w3.org/2000/svg', 'image'),
+            path = document.querySelector(id),
+            pos = self.getPosition(id);
 
-			if (document.body.contains(prevElement)) {
-				prevElement.remove();
-			}
-		}
+        if (!this.multiple) {
+            var prevElement = path.querySelector('image');
 
-		let markerPos = {};
+            if (document.body.contains(prevElement)) {
+                prevElement.remove();
+            }
+        }
 
-		if (url == null || height == null || width == null) {
-			url = this.marker.url;
-			height = this.marker.height;
-			width = this.marker.width;
-		}
+        let markerPos = {};
 
-		if (typeof x !== 'undefined' && typeof y !== 'undefined') {
-			markerPos = {
-				x: x,
-				y: y
-			};
-		} else {
-			markerPos = {
-				x: pos.x + pos.width / 2,
-				y: pos.y + pos.height / 2
-			};
-		}
+        if (url == null || height == null || width == null) {
+            url = this.marker.url;
+            height = this.marker.height;
+            width = this.marker.width;
+        }
 
-		path.appendChild(element);
+        if (typeof x !== 'undefined' && typeof y !== 'undefined') {
+            markerPos = {
+                x: x,
+                y: y
+            };
+        } else {
+            markerPos = {
+                x: pos.x + pos.width / 2,
+                y: pos.y + pos.height / 2
+            };
+        }
 
-		element.setAttributeNS(null, 'href', url);
-		element.setAttributeNS(null, 'class', 'mapMarker');
-		element.setAttributeNS(null, 'height', height);
-		element.setAttributeNS(null, 'width', width);
+        path.appendChild(element);
 
-		let imagesList = path.querySelectorAll('image');
+        element.setAttributeNS('http://www.w3.org/1999/xlink', 'xlink:href', url);
+        element.setAttributeNS(null, 'class', 'mapMarker');
+        element.setAttributeNS(null, 'height', height);
+        element.setAttributeNS(null, 'width', width);
 
-		const elementSize = element.getBBox();
+        let imagesList = path.querySelectorAll('image');
 
-		let point = {
-			x: markerPos.x - elementSize.width / 2,
-			y: markerPos.y - elementSize.height / 2
-		};
+        const elementSize = element.getBBox();
 
-		if (imagesList.length > 1) {
-			let part = pos.width / imagesList.length,
-				move = part - part / 2;
+        let point = {
+            x: markerPos.x - elementSize.width / 2,
+            y: markerPos.y - elementSize.height / 2
+        };
 
-			switch (this.multiple) {
-				case 'horizontal':
-					[ ...imagesList ].map((node, index) => {
-						// console.log();
+        if (imagesList.length > 1) {
+            let part = pos.width / imagesList.length,
+                move = part - part / 2;
 
-						let pointX = pos.x + part * (index + 1) - move;
+            switch (this.multiple) {
+                case 'horizontal':
+                    [...imagesList].map((node, index) => {
+                        // console.log();
 
-						node.setAttributeNS(null, 'x', pointX);
-						node.setAttributeNS(null, 'y', markerPos.y);
-					});
-					break;
-				case 'vertical':
-					[ ...imagesList ].map((node, index) => {
-						// console.log();
+                        let pointX = pos.x + part * (index + 1) - move;
 
-						let pointY = pos.y + part * (index + 1) - move;
+                        node.setAttributeNS(null, 'x', pointX);
+                        node.setAttributeNS(null, 'y', markerPos.y);
+                    });
+                    break;
+                case 'vertical':
+                    [...imagesList].map((node, index) => {
+                        // console.log();
 
-						node.setAttributeNS(null, 'x', point.x);
-						node.setAttributeNS(null, 'y', pointY);
-					});
-					break;
-				default:
-					break;
-			}
-		} else {
-			element.setAttributeNS(null, 'x', point.x);
-			element.setAttributeNS(null, 'y', markerPos.y);
-		}
-	};
+                        let pointY = pos.y + part * (index + 1) - move;
 
-	this.list = () => {
-		const regions = document.querySelectorAll('.region'),
-			regionNames = [ ...regions ].map((item) => {
-				return item.id;
-			});
+                        node.setAttributeNS(null, 'x', point.x);
+                        node.setAttributeNS(null, 'y', pointY);
+                    });
+                    break;
+                default:
+                    break;
+            }
+        } else {
+            element.setAttributeNS(null, 'x', point.x);
+            element.setAttributeNS(null, 'y', markerPos.y);
+        }
+    };
 
-		return regionNames.sort();
-	};
+    this.list = () => {
+        const regions = document.querySelectorAll('.region'),
+            regionNames = [...regions].map((item) => {
+                return item.id;
+            });
+
+        return regionNames.sort();
+    };
 };
